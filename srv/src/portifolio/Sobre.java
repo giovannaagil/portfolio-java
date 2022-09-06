3762ab758035dd6c0da79a292efcb9246802a4aa
Giovanna Gil <93685210+giovannaagil@users.noreply.github.com>
2022-09-05 21:32:30 -0300

    Add files via upload

---
+++ srv/src/portifolio/Sobre.java
@@ -0,0 +1,73 @@
+package portifolio;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.Color;
+
+public class Sobre extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sobre() {
+		getContentPane().setBackground(Color.WHITE);
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/sobre.png")));
+		setTitle("Sobre");
+		setModal(true);
+		setBounds(100, 100, 450, 418);
+		getContentPane().setLayout(null);
+
+		JTextPane txtpnSobre = new JTextPane();
+		txtpnSobre.setEditable(false);
+		txtpnSobre.setFont(new Font("Dialog", Font.PLAIN, 12));
+		txtpnSobre.setText(
+				"Ol\u00E1! Eu sou a Giovanna! \r\nTrabalho com Marketing e sou apaixonada por tecnologia.\r\nEstes s\u00E3o os meus primeiros passos em desenvolvimento de sistemas.\r\n\r\nHello! My name is Giovanna!\r\nI work with Marketing and I'm in love with technology.\r\nThese are my first steps in systems development.");
+		txtpnSobre.setBounds(83, 42, 253, 187);
+		getContentPane().add(txtpnSobre);
+
+		JLabel lblSobALicena = new JLabel("Sob a licen\u00E7a MIT");
+		lblSobALicena.setBounds(293, 279, 131, 15);
+		getContentPane().add(lblSobALicena);
+
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon(Sobre.class.getResource("/img/mit.png")));
+		lblLogo.setBounds(152, 228, 131, 128);
+		getContentPane().add(lblLogo);
+
+		JLabel lblSobre = new JLabel("Sobre");
+		lblSobre.setFont(new Font("Dialog", Font.BOLD, 24));
+		lblSobre.setBounds(182, 11, 84, 28);
+		getContentPane().add(lblSobre);
+
+		JLabel lblVersao = new JLabel("Vers\u00E3o 5.0");
+		lblVersao.setBounds(293, 305, 91, 15);
+		getContentPane().add(lblVersao);
+
+	}
+}
